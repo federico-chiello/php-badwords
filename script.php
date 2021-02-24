2935e5c6d3202271836ef2dd4f4af1fb9147fc95
Fede <chiellofederico@gmail.com>
2021-02-24 17:04:55 +0100

    utilizzo di strtolower()

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *. -->
 
 <!-- PHP -->
-<?php $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' ?>
+<?php $testo = strtolower('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.') ?>
 <?php $parola = $_GET['badword']; ?>
 <!-- HTML -->
 <!DOCTYPE html>
