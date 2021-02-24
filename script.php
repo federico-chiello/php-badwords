e82d79447826d799fca8fc9d600defab35993a0b
Fede <chiellofederico@gmail.com>
2021-02-24 16:25:26 +0100

    stampa del testo e della sua lunghezza

---
+++ script.php
@@ -0,0 +1,22 @@
+<!-- Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *. -->
+
+<!-- PHP -->
+<?php $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' ?>
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Badword</title>
+</head>
+<body>
+
+  <p><strong>PARAGRAFO:</strong> <?php echo $testo; ?></p>
+
+  <div><strong>LUNGHEZZA DEL TESTO:</strong> <?php echo strlen($testo); ?></div>
+
+</body>
+</html>
